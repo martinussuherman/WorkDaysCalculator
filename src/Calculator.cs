3c5178eea90984ecd90e005ef485362ca4e56880
Martinus Suherman <martinus.suherman@gmail.com>
2020-10-22 16:05:38 +0700

    Refactor and add more test

--- src/Calculator.cs
+++ src/Calculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace WorkDaysCalculator
@@ -8,15 +9,30 @@ namespace WorkDaysCalculator
         // https://stackoverflow.com/questions/1617049/calculate-the-number-of-business-days-between-two-dates
         public int GetWorkingDays(DateTime startDate, DateTime endDate)
         {
-            int numberOfWeeks = (endDate.Subtract(startDate).Days + 1) / 7;
+            int daySpan = endDate.Subtract(startDate).Days;
+            int numberOfWeeks = (daySpan + 1) / 7;
             int fullWeekWorkDays = numberOfWeeks * 5;
-            int remainderDays = endDate.Subtract(startDate).Days - (numberOfWeeks * 7);
+            int remainderDays = daySpan - (numberOfWeeks * 7);
             int remainderWorkDays = Enumerable
                 .Range(0, remainderDays + 1)
                 .Select(x => startDate.AddDays(x))
                 .Count(x => x.DayOfWeek != DayOfWeek.Saturday && x.DayOfWeek != DayOfWeek.Sunday);
 
             return fullWeekWorkDays + remainderWorkDays;
+        }
+
+        public int GetWorkingDays(
+            DateTime startDate,
+            DateTime endDate,
+            IEnumerable<DateTime> holidayList = null)
+        {
+            if (holidayList == null || holidayList.Count() == 0)
+            {
+                return GetWorkingDays(startDate, endDate);
+            }
+
+            // TODO : get holiday count and substract from working days
+            return GetWorkingDays(startDate, endDate);
         }
     }
 }
--- test/CalculatorUnitTest.cs
+++ test/CalculatorUnitTest.cs
@@ -13,7 +13,7 @@ namespace WorkDaysCalculatorTest
             int month = DateTime.Today.Month;
             int year = DateTime.Today.Year;
 
-            for (int day = 1; day <= 20; day++)
+            for (int day = 1; day <= 7; day++)
             {
                 DateTime start = new DateTime(year, month, day);
                 DateTime end = start.AddDays(6);
@@ -22,17 +22,93 @@ namespace WorkDaysCalculatorTest
         }
 
         [Fact]
-        public void GetWorkingDays_DateSpanIsTwoWeeks_Return_10()
+        public void GetWorkingDays_DateSpanIsThreeWeeks_Return_15()
         {
             Calculator calculator = new Calculator();
             int month = DateTime.Today.Month;
             int year = DateTime.Today.Year;
 
-            for (int day = 1; day <= 20; day++)
+            for (int day = 1; day <= 7; day++)
             {
                 DateTime start = new DateTime(year, month, day);
-                DateTime end = start.AddDays(13);
-                Assert.Equal(10, calculator.GetWorkingDays(start, end));
+                DateTime end = start.AddDays(20);
+                Assert.Equal(15, calculator.GetWorkingDays(start, end));
+            }
+        }
+
+        [Fact]
+        public void GetWorkingDays_SameDaysWorkdays_Return_1()
+        {
+            Calculator calculator = new Calculator();
+            DateTime mon = new DateTime(2020, 10, 5);
+            DateTime tue = new DateTime(2020, 10, 6);
+            DateTime wed = new DateTime(2020, 10, 7);
+            DateTime thu = new DateTime(2020, 10, 8);
+            DateTime fri = new DateTime(2020, 10, 9);
+
+            Assert.Equal(1, calculator.GetWorkingDays(mon, mon));
+            Assert.Equal(1, calculator.GetWorkingDays(tue, tue));
+            Assert.Equal(1, calculator.GetWorkingDays(wed, wed));
+            Assert.Equal(1, calculator.GetWorkingDays(thu, thu));
+            Assert.Equal(1, calculator.GetWorkingDays(fri, fri));
+        }
+
+        [Fact]
+        public void GetWorkingDays_SameDaysWeekend_Return_0()
+        {
+            Calculator calculator = new Calculator();
+            DateTime sat = new DateTime(2020, 10, 3);
+            DateTime sun = new DateTime(2020, 10, 4);
+
+            Assert.Equal(0, calculator.GetWorkingDays(sat, sat));
+            Assert.Equal(0, calculator.GetWorkingDays(sun, sun));
+        }
+
+        [Fact]
+        public void GetWorkingDays_SatToSun_Return_0()
+        {
+            Calculator calculator = new Calculator();
+            DateTime sat = new DateTime(2020, 10, 3);
+            DateTime sun = new DateTime(2020, 10, 4);
+
+            Assert.Equal(0, calculator.GetWorkingDays(sat, sun));
+        }
+
+        [Fact]
+        public void GetWorkingDays_SatToMon_Return_1()
+        {
+            Calculator calculator = new Calculator();
+            DateTime sat = new DateTime(2020, 10, 3);
+            DateTime mon = new DateTime(2020, 10, 5);
+
+            Assert.Equal(1, calculator.GetWorkingDays(sat, mon));
+        }
+
+        [Fact]
+        public void GetWorkingDays_WeekendToNextWeekend_Return_5()
+        {
+            Calculator calculator = new Calculator();
+            DateTime sat = new DateTime(2020, 10, 3);
+            DateTime sun = new DateTime(2020, 10, 4);
+            DateTime nextSat = new DateTime(2020, 10, 10);
+            DateTime nextSun = new DateTime(2020, 10, 11);
+
+            Assert.Equal(5, calculator.GetWorkingDays(sat, nextSat));
+            Assert.Equal(5, calculator.GetWorkingDays(sat, nextSun));
+            Assert.Equal(5, calculator.GetWorkingDays(sun, nextSat));
+            Assert.Equal(5, calculator.GetWorkingDays(sun, nextSun));
+        }
+
+        [Fact]
+        public void GetWorkingDays_WorkdaysToNextWorkdays_Return_6()
+        {
+            Calculator calculator = new Calculator();
+            DateTime start = new DateTime(2020, 10, 5);
+            DateTime end = new DateTime(2020, 10, 9);
+
+            for (DateTime date = start; date <= end; date = date.AddDays(1))
+            {
+                Assert.Equal(6, calculator.GetWorkingDays(date, date.AddDays(7)));
             }
         }
     }
